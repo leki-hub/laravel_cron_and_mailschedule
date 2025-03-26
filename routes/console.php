f0eb5f03bef3a28925116697dfa6a21a1392226c
Leki <lekisintole82@gmail.com>
2025-03-26 09:58:20 +0300

    Laravel 10+ sometimes does not automatically register scheduled tasks

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Kernel;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Registering an example artisan command
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Manually bind the scheduler to make schedule:list work
+$schedule = app(Schedule::class);
+app(Kernel::class)->schedule($schedule);
